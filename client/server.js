ca305558822363b75d5bd137b2a87c40cf7dbe10
Lior Ur <lioru@wix.com>
2018-05-10 15:26:54 +0300

    add ticker

--- client/server.js
+++ client/server.js
@@ -5,20 +5,26 @@ const methodOverride = require('method-override');
 const proxy = require('http-proxy-middleware');
 const apiRouter = require('./api-router');
 const config = require('./config');
-
+var cors = require('cors')
 
 function initServer() {
     const app = express();
     const router = express.Router();
+    app.use(cors())
     app.use(bodyParser.json());
     app.use(methodOverride());
     app.use(router);
     app.use(apiRouter());
 
-    router.use('/', proxy(
-        {
-            target: 'http://localhost:3000/',
-        }));
+    //router.use('/public', proxy({target: 'http://localhost:3000/',}));
+    //router.use('/static/js/bundle.js', proxy({target: 'http://localhost:3000/static/js/bundle.js',}));
+
+// // Any route should render the web app html (hosted by by Webpack-dev-server)
+//     router.use('**', proxy(
+//         {
+//             target: 'http://localhost:3000/',
+//             //           pathRewrite: path => '/public/index.html',
+//         }));
 
 
     const server = app.listen(config.SERVER_PORT, () => {
