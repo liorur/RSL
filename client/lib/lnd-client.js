fd11b8b2ce516d7aa0cb970e58aae1e6b59f9a63
Jacob Dvir <jacob.dvir@gmail.com>
2018-05-10 20:16:20 +0300

    basic send payment

--- client/lib/lnd-client.js
+++ client/lib/lnd-client.js
@@ -1,7 +1,9 @@
 console.log(require('dotenv').config({path: '../.env'}));
 const grpc = require('grpc'),
     fs = require('fs'),
-    lnrpcDescriptor = grpc.load('../rpc.proto');
+    lnrpcDescriptor = grpc.load('../rpc.proto'),
+    _ = require('lodash'),
+    ByteBuffer = require('bytebuffer');
 
 const certPath = `${process.env.LNSERVICE_LND_DIR}/tls.cert`,
     cert = fs.readFileSync(certPath),
@@ -46,8 +48,35 @@ function listChannels(active_only = true, inactive_only = false, public_only = f
     });
 }
 
+function sendPayment(destPubKey, amount) {
+    return new Promise((resolve, reject) => {
+        if (!destPubKey)
+            return reject(new Error('destPubKey must be provided.'));
+        if (!amount || amount <= 0 || !_.isInteger(amount))
+            return reject(new Error('amount must be a positive integer'));
+        let destPubKeyBytes = ByteBuffer.fromHex(destPubKey);
+
+        var call = lightning.sendPayment();
+        call.on('data', function (payment) {
+            console.log('Payment sent:');
+            console.log(payment);
+            call.end();
+            resolve(payment);
+        });
+        call.on('end', function () {
+            console.log('END');
+        });
+
+        call.write({
+            dest: destPubKeyBytes,
+            amt: amount,
+        });
+    });
+}
+
 module.exports = {
     getInfo,
     listPeers,
     listChannels,
+    sendPayment,
 };
--- tests/lnd-tests.js
+++ tests/lnd-tests.js
@@ -1,13 +1,17 @@
 const lndClient = require('../client/lib/lnd-client');
 
-lndClient.getInfo()
-    .then(info => console.log(info))
-    .catch(err => console.error(err));
+// lndClient.getInfo()
+//     .then(info => console.log(info))
+//     .catch(err => console.error(err));
+//
+// lndClient.listPeers()
+//     .then(info => console.log(info))
+//     .catch(err => console.error(err));
+//
+// lndClient.listChannels()
+//     .then(info => console.log(info))
+//     .catch(err => console.error(err));
 
-lndClient.listPeers()
-    .then(info => console.log(info))
-    .catch(err => console.error(err));
-
-lndClient.listChannels()
+lndClient.sendPayment(process.env.REMOTE_PUBKEY, 10)
     .then(info => console.log(info))
     .catch(err => console.error(err));
