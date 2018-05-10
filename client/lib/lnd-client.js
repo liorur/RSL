7d404e0025c4915aa09247e6d788b189f2eb3d45
Jacob Dvir <jacob.dvir@gmail.com>
2018-05-10 15:39:22 +0300

    use env variables

--- client/lib/lnd-client.js
+++ client/lib/lnd-client.js
@@ -1,19 +1,35 @@
+console.log(require('dotenv').config({path: '../../.env'}));
 const grpc = require('grpc'),
     fs = require('fs'),
     lnrpcDescriptor = grpc.load('../../rpc.proto');
 
-// Due to updated ECDSA generated tls.cert we need to let gprc know that
-// we need to use that cipher suite otherwise there will be a handhsake
-// error when we communicate with the lnd rpc server.
-process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA';
+const certPath = `${process.env.LNSERVICE_LND_DIR}/tls.cert`,
+    cert = fs.readFileSync(certPath),
+    credentials = grpc.credentials.createSsl(cert),
+    lightning = new lnrpcDescriptor.lnrpc.Lightning(process.env.LNDSERVICE_ADDRESS, credentials);
 
-var cert = fs.readFileSync('/Users/jacobdvir/Library/Application Support/Lnd/tls.cert');
-var credentials = grpc.credentials.createSsl(cert);
-var lightning = new lnrpcDescriptor.lnrpc.Lightning('127.0.0.1:10009', credentials);
+function getInfo() {
+    return new Promise((resolve, reject) => {
+        lightning.getInfo({}, function (err, response) {
+            if (err)
+                reject(err);
+            else
+                resolve(response);
+        });
+    });
+}
 
-lightning.getInfo({}, function (err, response) {
-    if (err)
-        console.error(err);
-    else
-        console.log('GetInfo:', response);
-});
+async function getIntoAsync() {
+    try {
+        return await getInfo();
+    } catch (e) {
+        throw e;
+    }
+}
+
+module.exports = {getInfo, getIntoAsync};
+
+getIntoAsync()
+    .then(info => console.log(info))
+    .catch(err => console.error(err));
+
